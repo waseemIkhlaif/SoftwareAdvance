6f7d3e044a47209ffe136fb5823dca35955f08e2
waseemikhlaif <waseem.ikhlaif@gmail.com>
2024-06-14 15:22:41 +0300

    reset Password

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -23,6 +23,54 @@ router.post('/register', async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 });
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ where: { email } });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials Email' });
+        }
+        const isMatch = await bcrypt.compare(user.password, password);
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid credentials Password' });
+            console.log(isMatch);
+        }
+        res.json({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(userModel),
+        });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+// user logout
+router.get('/logout', (req, res) => {
+    res.json({ message: 'Logged out successfully' });
+});
+//reset password
+router.post('/reset-password', async (req, res) => {
+    const { email, newPassword } = req.body;
+
+    if (!newPassword || !newPassword.match(/^(?=.*[a-zA-Z])(?=.*[0-9])/)) {
+        return res.status(400).json({ message: 'Password must contain both letters and numbers' });
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 5);
+    updateUserPasswordByEmail(email, hashedPassword, (err, result) => {
+        if (err) {
+            console.error('Error updating password:', err);
+            return res.status(500).json({ message: 'An error occurred while updating the password.' });
+        }
+
+        // Check if the password was successfully updated
+        if (result.affectedRows > 0) {
+            res.status(200).json({ message: 'Password changed successfully.' });
+        } else {
+            res.status(404).json({ message: 'User not found or password not updated.' });
+        }
+    });
+});
 
 // get all users
 router.get('/', async (req, res) => {
